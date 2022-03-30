2a69ecace9433dbfbd3616e3b568438eef65ce7d
Daniele Fioroni <dfioroni91@gmail.com>
2022-03-30 17:23:44 +0200

    Update addon.py

--- plugin.program.emustation-launcher/addon.py
+++ plugin.program.emustation-launcher/addon.py
@@ -1,17 +1,14 @@
-import os
 import xbmc
-import xbmcgui
 import xbmcaddon
 import subprocess
 
 __plugin__ = "emustation-launcher"
 __author__ = "toast"
-__url__ = "https://github.com/memob0x/osmc-emustation-addons/"
-__git_url__ = "https://github.com/memob0x/osmc-emustation-addons/"
+__url__ = "https://github.com/memob0x/kodi-retropie-addons/"
+__git_url__ = "https://github.com/memob0x/kodi-retropie-addons/"
 __credits__ = "toast"
 __version__ = "0.0.1"
 
-dialog = xbmcgui.Dialog()
-addon = xbmcaddon.Addon(id='plugin.program.emustation-launcher')
+xbmcaddon.Addon(id='plugin.program.emustation-launcher')
 
-subprocess.call(["sh", "/home/osmc/.kodi/addons/plugin.program.emustation-launcher/launcher.sh"])
+subprocess.call(["sh", "~/.kodi/addons/plugin.program.emustation-launcher/launcher.sh"])
