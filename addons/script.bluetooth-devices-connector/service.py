6eb03719737ce684d22d82ff15fe918e51c04a66
Daniele Fioroni <dfioroni91@gmail.com>
2022-04-26 18:11:05 +0200

    Update service.py

--- addons/script.bluetooth-devices-connector/service.py
+++ addons/script.bluetooth-devices-connector/service.py
@@ -1,13 +1,1 @@
-import xbmcaddon, xbmc, time
 
-from utils import deleteFile, writeFile
-
-addon = xbmcaddon.Addon("script.bluetooth-devices-connector")
-
-file = addon.getAddonInfo('path') + "/devs.txt"
-
-deleteFile(file)
-
-devs_raw = addon.getSettingString("devs")
-
-writeFile(file, devs_raw)
