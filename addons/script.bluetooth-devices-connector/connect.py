5ec3aee5d45191207b850bb63e6093e57f60546c
memob0x <dfioroni91@gmail.com>
2022-04-29 16:32:58 +0200

    enhancements

--- addons/script.bluetooth-devices-connector/connect.py
+++ addons/script.bluetooth-devices-connector/connect.py
@@ -1,45 +1,55 @@
-from utils import isDeviceState, exeBtCmd
+from utils import isDeviceState, exeBtCmd, log
 
 def attemptInteraction(dev, cmd, state):
 	def interact():
 		exeBtCmd(cmd, dev)
 
-	interact()
-
 	attempts = 0
 
 	while not isDeviceState(state, dev):
-		attempts += 1
-
 		if attempts == 3:
 			return False
 
 		interact()
+
+		attempts += 1
 
 	return True
 
 def connect(devs):
 	report = ""
 
-	init = False
+	hasIteratedOnce = False
 
 	for dev in devs:
-		connected = attemptInteraction(dev, "connect", "Connected")
+		log(__file__ + " trying to connect to " + dev)
 
-		if init:
+		isConnected = attemptInteraction(dev, "connect", "Connected")
+
+		if hasIteratedOnce:
 			report += "\n\n"
 
-		report += dev + " Connected: " + str(connected)
+		log(__file__ + " " + dev + " connection result: " + str(isConnected))
 
-		paired = attemptInteraction(dev, "pair", "Paired")
+		report += dev + " Connected: " + str(isConnected)
 
-		report += "\n" + dev + " Paired: " + str(paired)
+		log(__file__ + " trying to pair " + dev)
 
-		if connected and paired:
-			paired = attemptInteraction(dev, "trust", "Trusted")
+		isPaired = attemptInteraction(dev, "pair", "Paired")
 
-			report += "\n" + dev + " Trusted: " + str(paired)
+		log(__file__ + " " + dev + " pairing result: " + str(isPaired))
 
-		init = True
+		report += "\n" + dev + " Paired: " + str(isPaired)
+
+		if isConnected and isPaired:
+			log(__file__ + " trying to trust " + dev)
+
+			isTrusted = attemptInteraction(dev, "trust", "Trusted")
+
+			log(__file__ + " " + dev + " trust result: " + str(isTrusted))
+
+			report += "\n" + dev + " Trusted: " + str(isTrusted)
+
+		hasIteratedOnce = True
 
 	return report
--- addons/script.bluetooth-devices-connector/utils.py
+++ addons/script.bluetooth-devices-connector/utils.py
@@ -1,14 +1,33 @@
-from subprocess import PIPE, Popen
+from subprocess import check_output
+
+BOOL_DEBUG = False
+
+def log(string):
+	if BOOL_DEBUG:
+		print(string)
 
 def exeCmd(cmd):
 	try:
-	        with Popen(cmd, stdout=PIPE, stderr=None, shell=True) as process:
-        	        return process.communicate()[0].decode("utf-8")
-	except:
+		output = check_output(cmd, shell=True)
+
+		log(__file__ + " exeCmd produced output: " + output)
+
+		return output
+	except Exception as e:
+		log(__file__ + " exeCmd returned error: "+ str(e))
+
 		return ""
 
 def exeBtCmd(action, dev):
-	return exeCmd("sudo bluetoothctl " + action + " " + dev)
+	cmd = "sudo bluetoothctl " + action + " " + dev
+
+	log(__file__ + " executing bt command \"" + cmd + "\"")
+
+	return exeCmd(cmd)
 
 def isDeviceState(state, dev):
-	return state + ": yes" in exeBtCmd("info", dev)
+	result = state + ": yes" in exeBtCmd("info", dev)
+
+	log(__file__ + " " + dev + " " + state + " " + str(result))
+
+	return result
